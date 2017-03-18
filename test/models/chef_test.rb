0bb99d67c4a0a871a65500e9bf55bc338c22d9aa
Jerry Rowell <jerry.rowell@verizon.net>
2017-03-18 17:43:38 +0000

    add chef and recipe model vaidaton with test

---
+++ test/models/chef_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.new(chefname: "jerry", email: "jerry@example.com")
+  end
+  
+  test "should be valid" do
+    assert @chef.valid?
+  end
+  
+  test "name should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+  
+  test "name length should be less than 30 characters" do
+    @chef.chefname = "a" * 31
+    assert_not @chef.valid?
+  end
+  
+  test "email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+  
+  test "email should not be to long" do
+    @chef.email = "a" * 245 + "example.com"
+    assert_not @chef.valid?
+  end
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@eee.com R_TDD-DS@eee.hello.org user@example.com first.last@eem.au laura+joe@monk.cm]
+    valid_addresses.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, '#{valids.inspect} should be valid'
+    end
+  end
+  
+    test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_eee.org user.name@example. eee@i_am_.com foo@ee+aar.com]
+    invalid_addresses.each do |invalids|
+      @chef.email = invalids
+      assert_not @chef.valid?, '#{invalids.inspect} should be invalid'
+    end
+  end
+  
+    test "email address should be unique" do
+    dup_chef = @chef.dup
+    dup_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not dup_chef.valid?
+  end
+  
+    test "email should be lower case before hitting db" do
+    mixed_email = "John@Example.com"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email
+  end
+  
+end
---
+++ test/models/recpe_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  def setup
+    
+    @recipe = Recipe.new(name: "chicken parm", description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should't be more than 500 characters" do
+    @recipe.description= "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should't be less than 5 characters" do
+    @recipe.description = "a" * 3
+    assert_not @recipe.valid?
+  end
+end
