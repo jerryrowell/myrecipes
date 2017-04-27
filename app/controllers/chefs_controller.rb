b95e196f3ac4877b55ac4248594ade2fe0e05df5
Jerry Rowell <jerry.rowell@verizon.net>
2017-04-27 19:31:34 +0000

    Complete authentication system with admin and test

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,7 @@
 class ChefsController < ApplicationController
   before_action :set_chef, only: [:show, :edit, :update, :destroy]
   before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
   
   def index
     @chefs = Chef.paginate(page: params[:page], per_page: 5)
@@ -41,8 +42,10 @@ class ChefsController < ApplicationController
   def destroy
     
     @chef.destroy
-    flash[:danger] = "Chef and all associated recipes have been delete"
-    redirect_to chefs_path
+    if !@chef.admin?
+      flash[:danger] = "Chef and all associated recipes have been delete"
+      redirect_to chefs_path
+    end
   end
   
   
@@ -57,10 +60,17 @@ class ChefsController < ApplicationController
   end
   
   def require_same_user
-    if current_chef != @chef
+    if current_chef != @chef and !current_chef.admin?
       flash[:danger] = "You can only edit or delete your own account"
       redirect_to chefs_path
     end
   end
   
+  def require_admin
+    if logged_in? && !current_chef.admin?
+      flash[:danger] = "Only admin users can perform that action"
+      redirect_to root_path
+    end
+  end
 end
+
